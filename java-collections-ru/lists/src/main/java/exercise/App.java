8ce82de42de9f325d02f6e9000ba8f9c39707cc2
Ioann <iopilipchuk@gmail.com>
2023-07-29 18:07:53 +0300

    first exercise

---
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -0,0 +1,27 @@
+package exercise;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+// BEGIN
+public class App {
+  public static boolean scrabble(String letters, String word) {
+    String[] lettersArray = letters.toLowerCase().split("");
+    String[] lettersWord = word.toLowerCase().split("");
+
+    List<String> lettersList = new ArrayList<String>(Arrays.asList(lettersArray));
+    List<String> wordLettresList = new ArrayList<String>(Arrays.asList(lettersWord));
+
+
+    for (String letter : wordLettresList) {
+      int index = lettersList.indexOf(letter);
+      if (index < 0) {
+          return false;
+      }
+      lettersList.remove(index);
+    }
+    return true;
+  }
+}
+//END

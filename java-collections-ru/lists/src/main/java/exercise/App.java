b1c4b79cdfb0d2c67c8ed0aa041c2888bd52e234
@hexlet/cli <support@hexlet.io>
2023-07-29 18:12:57 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -6,22 +6,22 @@ import java.util.List;
 
 // BEGIN
 public class App {
-  public static boolean scrabble(String letters, String word) {
-    String[] lettersArray = letters.toLowerCase().split("");
-    String[] lettersWord = word.toLowerCase().split("");
+    public static boolean scrabble(String letters, String word) {
+        String[] lettersArray = letters.toLowerCase().split("");
+        String[] lettersWord = word.toLowerCase().split("");
 
-    List<String> lettersList = new ArrayList<String>(Arrays.asList(lettersArray));
-    List<String> wordLettresList = new ArrayList<String>(Arrays.asList(lettersWord));
+        List<String> lettersList = new ArrayList<String>(Arrays.asList(lettersArray));
+        List<String> wordLettresList = new ArrayList<String>(Arrays.asList(lettersWord));
 
 
-    for (String letter : wordLettresList) {
-      int index = lettersList.indexOf(letter);
-      if (index < 0) {
-          return false;
-      }
-      lettersList.remove(index);
+        for (String letter : wordLettresList) {
+            int index = lettersList.indexOf(letter);
+            if (index < 0) {
+                return false;
+            }
+            lettersList.remove(index);
+        }
+        return true;
     }
-    return true;
-  }
 }
 //END
